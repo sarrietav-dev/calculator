f3927c38077890b4cd59e4980c61b9b94db4cdcb
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-02 15:34:58 -0500

    Added redux store

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
 
-const store = createStore();
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
