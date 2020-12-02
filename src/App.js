650132650c737b79e4de29936ca1aacf337108d2
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-02 00:57:51 -0500

    Added Context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
+import { CalcProvider } from "./components/context/CalcContext";
 import NumPad from "./components/NumPad";
 import Screen from "./components/Screen";
 
 function App() {
     return (
-        <div className="App">
-            <div className="calculator">
-                <Screen />
-                <NumPad />
+        <CalcProvider>
+            <div className="App">
+                <div className="calculator">
+                    <Screen />
+                    <NumPad />
+                </div>
             </div>
-        </div>
+        </CalcProvider>
     );
 }
 
