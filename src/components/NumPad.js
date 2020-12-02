e456d6b6ec7c96bfa514d0c4e5ffa76682490783
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-02 00:37:52 -0500

    Added id's to numpad's components

--- src/components/NumPad.js
+++ src/components/NumPad.js
@@ -5,23 +5,23 @@ import Operator from "./Operator";
 function NumPad() {
     return (
         <div className="numpad">
-            <Operator sign="AC" id="AC" />
-            <Operator sign="/" />
-            <Operator sign="X" />
-            <Number num={7} />
-            <Number num={8} />
-            <Number num={9} />
-            <Operator sign="-" />
-            <Number num={4} />
-            <Number num={5} />
-            <Number num={6} />
-            <Operator sign="+" />
-            <Number num={1} />
-            <Number num={2} />
-            <Number num={3} />
+            <Operator sign="AC" id="clear" />
+            <Operator sign="/" id="divide" />
+            <Operator sign="X" id="multiply" />
+            <Number num={7} id="seven" />
+            <Number num={8} id="eight" />
+            <Number num={9} id="nine" />
+            <Operator sign="-" id="subtract" />
+            <Number num={4} id="four" />
+            <Number num={5} id="five" />
+            <Number num={6} id="six" />
+            <Operator sign="+" id="add" />
+            <Number num={1} id="one" />
+            <Number num={2} id="two" />
+            <Number num={3} id="three" />
             <Operator sign="=" id="equals" />
-            <Number num={0} id="cero" />
-            <Operator sign="." id="dot" />
+            <Number num={0} id="zero" />
+            <Operator sign="." id="decimal" />
         </div>
     );
 }
