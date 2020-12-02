579017e08c2853fa3bfecd64e7002487140e8bec
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-02 00:38:27 -0500

    Added screen component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NumPad from "./components/NumPad";
+import Screen from "./components/Screen";
 
 function App() {
     return (
         <div className="App">
             <div className="calculator">
+                <Screen />
                 <NumPad />
             </div>
         </div>
---
+++ src/components/Screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Screen() {
+    return (
+        <div className="display-wrapper">
+            <div className="formula"></div>
+            <div id="display"></div>
+        </div>
+    );
+}
+
+export default Screen;
