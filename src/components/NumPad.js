0175219db9239b3deb83e7197042184e7e6dae41
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-02 15:35:35 -0500

    Changed numbers from int to str

--- src/components/NumPad.js
+++ src/components/NumPad.js
@@ -8,19 +8,19 @@ function NumPad() {
             <Operator sign="AC" id="clear" />
             <Operator sign="/" id="divide" />
             <Operator sign="X" id="multiply" />
-            <Number num={7} id="seven" />
-            <Number num={8} id="eight" />
-            <Number num={9} id="nine" />
+            <Number num={"7"} id="seven" />
+            <Number num={"8"} id="eight" />
+            <Number num={"9"} id="nine" />
             <Operator sign="-" id="subtract" />
-            <Number num={4} id="four" />
-            <Number num={5} id="five" />
-            <Number num={6} id="six" />
+            <Number num={"4"} id="four" />
+            <Number num={"5"} id="five" />
+            <Number num={"6"} id="six" />
             <Operator sign="+" id="add" />
-            <Number num={1} id="one" />
-            <Number num={2} id="two" />
-            <Number num={3} id="three" />
+            <Number num={"1"} id="one" />
+            <Number num={"2"} id="two" />
+            <Number num={"3"} id="three" />
             <Operator sign="=" id="equals" />
-            <Number num={0} id="zero" />
+            <Number num={"0"} id="zero" />
             <Operator sign="." id="decimal" />
         </div>
     );
