9fa9cdde9a4f4b3800a51f88b36151d7fb052498
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-02 15:21:00 -0500

    Deleted provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
-import { CalcProvider } from "./components/context/CalcContext";
 import NumPad from "./components/NumPad";
 import Screen from "./components/Screen";
 
 function App() {
     return (
-        <CalcProvider>
-            <div className="App">
-                <div className="calculator">
-                    <Screen />
-                    <NumPad />
-                </div>
+        <div className="App">
+            <div className="calculator">
+                <Screen />
+                <NumPad />
             </div>
-        </CalcProvider>
+        </div>
     );
 }
 
